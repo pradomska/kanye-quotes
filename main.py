445f93fcdd39db4d9897f5c4f9dc45931a24494e
Paulina Radomska <paulinaradomska10@wp.pl>
2023-04-25 14:20:25 +0200

    function to get quote

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tkinter import *
+import requests
 
 
 def get_quote():
-    pass
-    # Write your code here.
+    respone = requests.get("https://api.kanye.rest")
+    respone.raise_for_status()
+    canvas.itemconfig(quote_text, text=respone.json()["quote"])
 
 
 window = Tk()
@@ -11,14 +13,13 @@ window.title("Kanye Says...")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
+background_img = PhotoImage(file=r"images\background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"),
-                                fill="white")
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 22, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_img = PhotoImage(file=r"images\kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote, relief="flat")
 kanye_button.grid(row=1, column=0)
 
 window.mainloop()
